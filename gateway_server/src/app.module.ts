131c09058a27dec07922a86d0a635c2a1582b443
gimgangseob <rlarkdtjq590@naver.com>
2025-05-16 16:38:44 +0900

    [Update] Config

--- gateway_server/src/app.module.ts
+++ gateway_server/src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import configuration from './config/configuration';
+import { validationSchema } from './config/validation';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    // 환경변수 설정
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      validationSchema,
+    }),
+
+    // Mongo 연결
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const uri = configService.get<string>('mongoUri');
+        if (!uri) {
+          console.error('❌ MONGO_URI is not set!');
+          process.exit(1);
+        }
+        return {
+          uri,
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
